2c643e8ed53be10631fd02faf125a8718011a6f6
Samuel Bryan <s6muelbryan@gmail.com>
2022-02-03 22:32:20 -0200

    backgrounds,iframe e botão de ligar

--- CELL/inter.js
+++ CELL/inter.js
@@ -1,3 +1,12 @@
+setTimeout(() => {
+
+    const newstate = document.getElementById("newstate")        //animação float
+    newstate.style.opacity = "1"
+
+}, 2000);
+
+
+
 var h = new Date()
 var hora = h.getHours()
 var j = new Date()
@@ -65,22 +74,137 @@ function home(){
     const botmenu = document.getElementById("butt-menu")
     const wi = document.getElementById("wifi")
 
+    const you = document.getElementById("you")
+    const maps = document.getElementById("maps")
+
 
     mn.style.visibility = "hidden"
     cs.style.visibility = "hidden"
     botmenu.style.filter = "none"
     wi.style.display = "none"
 
+    you.style.display = "none"
+    maps.style.display = "none"
+}
+
+function cam(){
+    const permission = document.getElementById("permission")
+    permission.style.display = "flex"
+
+}
+
+function negarcam(){
+    const permission = document.getElementById("permission")
+    permission.style.display = "none"
+}
 
 
+function onmaps(){
+    const maps = document.getElementById("maps")
+    maps.style.display = "block"
+}
+function onyou(){
+    const you = document.getElementById("you")
+    you.style.display = "block"
 }
 
 
 
-setTimeout(() => {
 
-    const newstate = document.getElementById("newstate")
-    newstate.style.opacity = "1"
 
-}, 2000);
+
+
+
+
+
+
+
+function backcell(){
+    const display = document.getElementById("display")
+    display.style.backgroundImage = "url(assets/wall.jpg)"
+}
+function backcell2(){
+    const display = document.getElementById("display")
+    display.style.backgroundImage = "url(assets/fund2.jpg)"             //background do celular
+}
+function backcell3(){
+    const display = document.getElementById("display")
+    display.style.backgroundImage = "url(assets/wall2.jpg)"
+}
+
+
+function oncell(){
+    const float = document.getElementById("float")
+    const ondisplay = document.getElementById("ondisplay")
+    const notify = document.getElementById("notify")
+
+    
+    if(float.style.right != "25%"){
+        float.style.right = "25%"
+        ondisplay.style.display = "none"
+        notify.style.display = "flex"
+    } else{
+        float.style.right = "90%"
+        ondisplay.style.display = "block"
+        notify.style.display = "none"
+
+
+    }
+}
+
+
+
+function backtale(){
+    document.body.style.backgroundImage = "url(assets/tale/back1.jpg)"
+
+    const post = document.getElementById("newstate")
+    post.style.borderColor = "#ecf0f1"
+
+    const secbackcell = document.getElementById("backgrounds-cell")
+    secbackcell.style.borderColor = "#c9c9c9"
+
+    const on = document.getElementById("on")
+    on.style.borderColor = "#c9c9c9"
+}
+function backtale2(){
+    document.body.style.backgroundImage = "url(assets/tale/back2.jpg)"
+
+    const post = document.getElementById("newstate")
+    post.style.borderColor = "#c9d6dc"
+
+    const secbackcell = document.getElementById("backgrounds-cell")
+    secbackcell.style.borderColor = "#000"
+
+    const on = document.getElementById("on")
+    on.style.borderColor = "#383838be"
+}
+function backtale3(){                                                                       //background da tela 
+    document.body.style.backgroundImage = "url(assets/tale/back3.png)"
+
+    const post = document.getElementById("newstate")
+    post.style.borderColor = "#1ac7ff"
+
+    const secbackcell = document.getElementById("backgrounds-cell")
+    secbackcell.style.borderColor = "#1ac7ff"
+
+    const on = document.getElementById("on")
+    on.style.borderColor = "#1ac7ff"
+
+
+}
+function backtale4(){
+    document.body.style.backgroundImage = "url(assets/fund3.jpg)"
+
+    const post = document.getElementById("newstate")
+    post.style.borderColor = "#fb9b2a"
+
+    const secbackcell = document.getElementById("backgrounds-cell")
+    secbackcell.style.borderColor = "#070707"
+
+    const on = document.getElementById("on")
+    on.style.borderColor = "#383838be"
+}
+
+
+
 
